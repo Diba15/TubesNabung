19ad2b6dbcb9facaa23a2927d543f1a0786b9505
DimasBagasSaputro <dimazzbagazz@yahoo.co.id>
2021-04-27 19:28:54 +0700

    add handler on splash screen

--- app/src/main/java/com/d3if2089/contohpt2/SplashActivity.kt
+++ app/src/main/java/com/d3if2089/contohpt2/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.d3if2089.contohpt2
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -9,9 +10,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        Handler().postDelayed({
+            kotlin.run {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        },2500)
 
     }
 }
